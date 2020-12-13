6b4171c21cf4c38938eedde1ce71c2952f0a1167
mlapinm <mlapin@rambler.ru>
2020-12-13 17:30:59 +0300

    Part 1: Setting Up our Application

--- f4012fcc/app.js
+++ f4012fcc/app.js
@@ -1,9 +1,22 @@
-const fs = require('fs')
-// deleted the file
-fs.unlink('example2.txt', (err)=>{
-  if(err){
-    console.log(err)
-  } else{
-    console.log('successfully deleted the file')
-  }
+const express = require('express')
+const path = require('path')
+const Joi = require('joi')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use('/public', express.static(path.join(__dirname,'static')))
+app.set('view engine', 'ejs')
+
+app.get('/:userQuery', (req, res)=>{
+  res.render('index', {data: {userQuery: req.params.userQuery,
+                      searchResults: ['book1', 'book2', 'book3', ],
+                      loggedIn: true,
+                      username: 'Pedro'
+                      }
+  })
 })
+
+console.log('listen 3000...')
+app.listen(3000)
+
