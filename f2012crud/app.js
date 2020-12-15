10724334d29c2801ec261cef232223a0ba343154
mlapinm <mlapin@rambler.ru>
2020-12-15 08:58:18 +0300

    2

--- f2012crud/app.js
+++ f2012crud/app.js
@@ -7,6 +7,43 @@ const path = require('path')
 const db = require('./db')
 const collection = "todo"
 
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/getTodos', (req, res)=>{
+  db.getDB().collection(collection).find({}).toArray((err, documents)=>{
+    if(err){
+      console.log(err)
+    }else{
+      console.log(documents)
+      res.json(documents)
+    }
+  })
+})
+
+app.put('/:id', (req, res)=>{
+  const todoID = req.params.id
+  const userInput = req.body
+
+  // db.getDB().collection(collection)
+  // .findOneAndUpdate({_id : todoID}, {$set : {todo : userInput.todo}, {returnOriginal : false}, (err, result)=>{
+    
+  // }})
+  db.getDB().collection(collection).findOneAndUpdate(
+    {_id : todoID},
+    {$set : {todo : userInput.todo}}, 
+    {returnOriginal : false}, (err, result)=>{
+      if(err){
+        console.log(err)
+      }else{
+        console.log(result)
+        res.json(result)
+      }
+    }
+  )
+})
+
 db.connect((err)=>{
   if(err){
     console.log('unable to connect to database')
