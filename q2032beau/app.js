c320ac3633616866c8f310b49e3e9db8445d187a
mlapinm <mlapin@rambler.ru>
2020-12-20 09:26:47 +0300

    part 7 fetch get

---
+++ q2032beau/app.js
@@ -0,0 +1,11 @@
+$(()=>{
+  // $("p").click(()=>{
+  //   console.log("You clicked a paragraph...")
+  // })
+
+  $("p").mouseleave(()=>{
+    console.log("You clicked a paragraph...")
+  })
+})
+// dblclick, mouseenter, mouseleave, 
+// mousedown, mouseup, hover
